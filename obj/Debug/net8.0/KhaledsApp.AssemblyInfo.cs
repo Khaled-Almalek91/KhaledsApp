5f7bd90eada1c69fdad4ed0ac1a9398ffb4c4c2f
Khaled-Almalek91 <khaled_king1991@hotmail.com>
2024-04-23 08:23:54 +0200

    push the app

--- obj/Debug/net8.0/KhaledsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/KhaledsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KhaledsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdff764c7f3745a7bed28206071250b75317ac13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3195601a1c27154c78512d91d9afafb7f1d0f5bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("KhaledsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KhaledsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
