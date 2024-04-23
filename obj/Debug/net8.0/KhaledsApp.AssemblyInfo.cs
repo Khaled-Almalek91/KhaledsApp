32d9deb23a45024549b61095873c47b91ce570cd
Khaled-Almalek91 <khaled_king1991@hotmail.com>
2024-04-23 08:51:16 +0200

    new add provison+cloudinit

--- obj/Debug/net8.0/KhaledsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/KhaledsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KhaledsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3195601a1c27154c78512d91d9afafb7f1d0f5bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ae1f9bab86d1a934444e975766a4e1057d007c")]
 [assembly: System.Reflection.AssemblyProductAttribute("KhaledsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KhaledsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
